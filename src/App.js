5902de88ac9e23db49c06714e7dd2c29fb96394d
Jessica Bray <49332535+GeniusMind2001@users.noreply.github.com>
2020-04-28 14:34:16 +1000

    week 3 content
    week 3 content downloaded from Codesandbox

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+export default function App() {
+  function LikeCounter() {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+      setCount(oldCount => oldCount + 1);
+    };
+    const decrement = () => {
+      setCount(oldCount => oldCount - 1);
+    };
+    return (
+      <div>
+        <p>Like Count: {count}</p>
+        <button onClick={increment}>Like</button>
+        <button onClick={decrement}>Dislike</button>
+      </div>
+    );
+  }
+}
